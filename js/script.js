482bc9dd12fe1b1238f8cce96158fce49c03ea88
arunsla <arunraj.softlogic@gmail.com>
2025-10-24 12:53:18 +0530

    js day 1

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// console.log("ar");
+// // console.log(34567=);
+
+
+// variables 
+// data types 
+// operators 
+// functions
+// conditions 
+// loops
+// comments 
+
+
+
+
+// data types
+// String
+// Integer 
+// Float 
+// bigint 
+// boolean
+// array 
+// object 
+// null or undefined 
+// symboll
+
+
+
+
+// let,  var , const 
+
+
+// var   // 2015
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
