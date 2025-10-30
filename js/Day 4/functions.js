e82dbf9152d9c3c7f39e05b3d06735f439b4cc3b
arunsla <arunraj.softlogic@gmail.com>
2025-10-30 12:56:30 +0530

    day 4 js

---
+++ js/Day 4/functions.js
@@ -0,0 +1,134 @@
+// defined using function keyword
+
+// // defining  function syntax
+// function functName (){
+//     // code block
+// }
+
+// functName();  // calling a function
+
+// function greet() {
+//   console.log("hello welcome to js !");
+// }
+
+// greet();
+
+// function greetUser(name) {
+//   console.log("hello " + name + " come");
+// }
+
+// greetUser("Aarthi")
+
+// function add (a,b){
+//     return a + b
+// }
+
+// let sum = add(10 , 5);
+
+// console.log(sum);
+
+// function sayHi(){
+
+// }
+
+// const sayHi = function(a, b ) {
+//     return a + b
+// }
+
+// console.log( sayHi(10 , 9 ));
+
+// arrow function
+
+// const sayHi = () => {
+
+// }
+
+// function hello (){
+//     console.log('hello !');
+
+// }
+
+// const greet = function(){
+//     console.log("hi there !");
+
+// }
+
+// hello()
+// greet()
+
+// const functionName = (parameters) => expression;
+
+// const greet = () => console.log(" Hello i am a arrow function !");
+// greet()
+
+// function greet(name){
+//     console.log("hello "+ name);
+// }
+
+// function userInput(callback) {
+//     let name = "rahul";
+//     callback(name)
+// }
+
+// userInput(greet)
+
+// let value = 1;
+
+// function doSomething(callback){
+//     callback();
+// }
+
+//  doSomething(() => {
+//   value = 2;
+// });
+
+// console.log(value);
+
+// function sayHello() {
+//   console.log("Hello !");
+// }
+
+// function greetUser(callback) {
+//   console.log("Fetching user data......");
+
+//   // delay
+
+//   callback();
+//   setTimeout(() => {console.log("user data fetched !");
+
+//   }, 5000);
+// }
+
+// greetUser(sayHello);
+
+// IIFE
+
+// (function(){
+//     console.log("this runs immediatly");
+
+// })
+
+// scope i a function
+
+// let globe = " global";
+
+// function scowScope() {
+//   var localScope = "local";
+
+//   console.log(globe);
+//   console.log(localScope);
+// }
+
+// console.log(localScope);
+
+// scowScope();
+
+
+
+
+
+
+
+
+
+
