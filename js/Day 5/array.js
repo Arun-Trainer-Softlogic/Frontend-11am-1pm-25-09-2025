2177b3fabb8801aaa06fe889ebe484b7ec05dcec
arunsla <arunraj.softlogic@gmail.com>
2025-10-31 17:31:31 +0530

    Day 5

---
+++ js/Day 5/array.js
@@ -0,0 +1,139 @@
+// array
+
+// let fruits = [ "apple" , "Banana" , "Mango" , " orange"]
+
+// console.log(fruits[3]);
+
+// console.log(fruits);
+
+// fruits[1] = "cherry";
+
+// console.log(fruits);
+
+// console.log(fruits.length);
+
+// fruits.push("Grapes"); // adds to the end
+// fruits.pop();
+// fruits.shift() ;
+// fruits.unshift("Grapes");  // add to start
+
+// console.log(fruits);
+
+// let colors = ["red", "Green", "Blue", "Purple", "black"];
+
+//  colors.forEach(function (color){
+//     console.log(color);
+
+//  })
+
+// let numbers = [1, 2, 3, 4, 5];
+
+//  let sq = numbers.map(nu => nu * nu);
+
+//  console.log(sq);
+
+// let ages = [12, 18 , 25, 30] ;
+// let adult = ages.filter(age => age >= 18)
+
+// console.log(adult);
+
+// let colors = ["red", "Green", "Blue", "Purple", "black"];
+
+// // let rr = colors.filter()
+
+// let blue = colors.filter(bl => bl === "Blue" || bl === "Purple");
+
+// console.log(blue);
+
+// let matrix = [
+
+//     [1,2,3],
+//     [4,5,[22,33,44,[555,6666,777,888]]],
+//     [7,8,[6789 , [000, [00 , 0, [99]]]],]
+// ]
+
+// console.log(matrix[2][2][1][1][2][0]);
+
+// let matrix = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9]
+// ];
+
+// for (let i = 0 ; i < matrix.length ; i++){
+//     for (let j = 0 ; j < matrix[i].length; j++){
+//         console.log(matrix[i][j]);
+
+//     }
+// }
+
+// let fruits = ["apple", "Banana", "Cherry"]
+
+// console.log(fruits[2]);
+
+// console.log("total Fruits : " , fruits.length);
+
+// let colors = ["Red", "Green"];
+
+// let push = colors.push("blue");  
+
+// console.log(colors); // add in last 
+
+// let unshift = colors.unshift("Yellow");
+
+// console.log(colors); // adds in first 
+
+// let pop = colors.pop();
+// console.log(colors); // remove last 
+
+// let shift = colors.shift();
+
+// console.log(colors); //  remove first  
+
+
+
+
+
+
+
+
+// let numbers = [2,4,6,8,6,9,10,0]
+
+// numbers.forEach(n => console.log("Value : " ,n));
+
+
+// let doubled = numbers.map(n => n * 2);
+
+// console.log("Doubled : " , doubled);
+
+
+
+// let even = numbers.filter(n => n % 2 );
+
+// console.log(even);
+
+// console.log("filtered : ",even);
+
+
+
+  let students = [
+    ["Aditi", 21],
+    ["Rahul", 22],
+    ["Priya", 23]
+  ];
+
+
+// console.log(students[0][0] ,"is ", students[0][1], "years old" );
+
+for (let i = 0 ;  i < students.length ; i++){
+    console.log(students[i][0] , " is" , students[i][1] , "years old");
+}
+
+
+
+
+
+
+
+
+
