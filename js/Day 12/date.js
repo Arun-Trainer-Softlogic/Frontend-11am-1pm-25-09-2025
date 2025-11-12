780aa1a3604302059bc3be925a12c703eddf2742
arunsla <arunraj.softlogic@gmail.com>
2025-11-12 14:28:53 +0530

    day 12 js

---
+++ js/Day 12/date.js
@@ -0,0 +1,14 @@
+let today  = new Date;
+
+let date = today.getDate();
+let year = today.getFullYear();
+let month = today.getMonth();
+
+console.log(today);
+console.log(date);
+console.log(year);
+console.log(month + 1);
+
+
+
+
