6a14ef95f48648dd5dbf22dd7c9388d8c4e89bb7
arunsla <arunraj.softlogic@gmail.com>
2025-11-17 12:33:29 +0530

    update day 1 jqueryt

---
+++ sql/Day 1/ar.sql
@@ -0,0 +1,27 @@
+-- create database ar;
+
+-- use ar;
+
+
+-- create table employees (
+-- emp_id Int primary key auto_increment,
+-- first_name varchar(50),
+-- salary decimal(5,3)
+-- );
+--  select * from employees;
+
+--  insert into employees (first_name, salary)
+-- values ("patel",13.11)
+
+
+-- delete from employees where emp_id = 4;
+
+
+-- drop database ar;
+
+
+
+
+
+
+
